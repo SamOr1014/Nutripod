c1e059c92502c5f75d1171978a5549a5110f19e5
SamOr1014 <orchamsumsam1014@gmail.com>
2022-09-08 16:20:19 +0800

    merge changes of dietitian main

--- App/src/components/UI/DietitianUI/Dietitian_Main.tsx
+++ App/src/components/UI/DietitianUI/Dietitian_Main.tsx
@@ -1,22 +1,12 @@
 import { Center, Flex, Wrap } from "@chakra-ui/react";
 import { useState } from "react";
-<<<<<<< HEAD
-import "../../../../CSS/Calendar.css";
-=======
->>>>>>> 4bfc492270ba25fbd5dfe5a9b75ee2bcc93671b6
 
 export default function DietitianMain() {
   const [selectedDate, setSelectedDate] = useState(new Date());
   return (
     <>
       <Flex flexWrap="wrap" width="100%" mr="2">
-<<<<<<< HEAD
-        <Flex flex="1" max-height="100%" flexDir="column">
-          
-        </Flex>
-=======
         <Flex flex="1" max-height="100%" flexDir="column"></Flex>
->>>>>>> 4bfc492270ba25fbd5dfe5a9b75ee2bcc93671b6
         <Flex flex="1" height="100%" flexDirection="column" ml="2">
           <Center mb="5">
             <h1>{selectedDate.toLocaleDateString()}</h1>
