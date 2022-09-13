3cc7f30ef4eb80f3340327f1912f64cc132242bc
Roy Chan <roychan170@gmail.com>
2022-09-13 12:32:15 +0800

    init seed data and completed second migrations

--- api/migrations/20220913032219_second.ts
+++ api/migrations/20220913032219_second.ts
@@ -7,7 +7,9 @@ const professionTableName = "profession_types"
 const educationName = "education"
 const chronicConditionTableName = "chronic_condition"
 const userStatsTableName = "users_stats"
+const userWeightTableName = "users_weight"
 const dietitianReportTableName = "dietitian_reports"
+const followupTableName = "follow_up"
 const exercisesTableName = "users_exercises"
 const exercisesTypesTableName = "exercises_types"
 const bookingTableName = "booking"
@@ -25,33 +27,59 @@ const foodTableName = "food"
 
 export async function up(knex: Knex): Promise<void> {
 
+    await knex.schema.dropTableIfExists(usersDietTableName)
+    await knex.schema.dropTableIfExists(foodTableName)
+    await knex.schema.dropTableIfExists(foodGroupsTableName)
+    await knex.schema.dropTableIfExists(dietTypesTableName)
+    await knex.schema.dropTableIfExists(commentsTableName)
+    await knex.schema.dropTableIfExists(postTableName)
+    await knex.schema.dropTableIfExists(usersBloodGlucoseTableName)
+    await knex.schema.dropTableIfExists(usersBloodPressureTableName)
+    await knex.schema.dropTableIfExists(dietitianReportTableName)
+    await knex.schema.dropTableIfExists(bookingTableName)
+    await knex.schema.dropTableIfExists(timeSlotTableName)
+    await knex.schema.dropTableIfExists(exercisesTableName)
+    await knex.schema.dropTableIfExists(exercisesTypesTableName)
+    await knex.schema.dropTableIfExists(userWeightTableName)
+    await knex.schema.dropTableIfExists(userStatsTableName)
+    await knex.schema.dropTableIfExists(userTableName)
+    await knex.schema.dropTableIfExists(followupTableName)
+    await knex.schema.dropTableIfExists(chronicConditionTableName)
+    await knex.schema.dropTableIfExists(dietitianTableName)
+    await knex.schema.dropTableIfExists(educationName)
+    await knex.schema.dropTableIfExists(professionTableName)
+    await knex.schema.dropTableIfExists(genderTableName)
+
+
+    
     await knex.schema.createTable(professionTableName, (table) => {
         table.increments()
         table.text('pf_type').notNullable()
     })
-
+   
     await knex.schema.createTable(genderTableName, (table) => {
         table.increments()
         table.text('gender_type').notNullable()
     })
 
-
+    
     await knex.schema.createTable(educationName, (table) => {
         table.increments()
         table.text('education_level').notNullable()
     })
 
+   
     await knex.schema.createTable(chronicConditionTableName, (table) => {
         table.increments()
         table.text('disease').notNullable()
     })
-
+    
     await knex.schema.createTable(exercisesTypesTableName, (table) => {
         table.increments()
         table.string('ex_type').notNullable()
         table.decimal('ex_calories', 8, 4).notNullable()
     })
-
+   
     await knex.schema.createTable(userTableName, (table) => {
         table.increments()
         table.string('username').unique().notNullable()
@@ -60,8 +88,8 @@ export async function up(knex: Knex): Promise<void> {
         table.string('email').unique().nullable()
         table.string('password').notNullable()
         table.date('birthday').notNullable()
-        table.integer('height').notNullable()
-        table.integer('weight').notNullable()
+        table.float('height').notNullable()
+        table.float('weight').notNullable()
         table.integer('gender').unsigned().notNullable()
         table.foreign('gender').references('gender.id')
         table.string('phone', 8).unique().notNullable()
@@ -69,14 +97,14 @@ export async function up(knex: Knex): Promise<void> {
         table.integer('profession').unsigned().notNullable()
         table.foreign('profession').references('profession_types.id')
         table.string('hkid', 8).unique().notNullable()
-        table.integer('chronic_condition').unsigned()
+        table.integer('chronic_condition').unsigned().nullable()
         table.foreign('chronic_condition').references('chronic_condition.id')
         table.integer('education').unsigned().notNullable()
         table.foreign('education').references('education.id')
         table.boolean('is_deleted').notNullable().defaultTo(false)
         table.timestamps(false, true)
     })
-
+   
     await knex.schema.createTable(dietitianTableName, (table) => {
         table.increments()
         table.string('username').unique().notNullable()
@@ -87,7 +115,7 @@ export async function up(knex: Knex): Promise<void> {
         table.timestamps(false, true)
         table.boolean('is_deleted').notNullable().defaultTo(false)
     })
-
+   
     await knex.schema.createTable(exercisesTableName, (table) => {
         table.increments()
         table.date('date').notNullable()
@@ -96,16 +124,17 @@ export async function up(knex: Knex): Promise<void> {
         table.decimal('duration', 4, 2).notNullable()
         table.timestamps(false, true)
         table.boolean('is_deleted').defaultTo(false)
-        table.integer('users_id').unsigned().notNullable()
-        table.foreign('users_id').references('users.id')
+        table.integer('user_id').unsigned().notNullable()
+        table.foreign('user_id').references('users.id')
     })
-
+    
+    
     await knex.schema.createTable(timeSlotTableName, (table) => {
         table.increments()
         table.time('time').unique().notNullable()
     })
 
-
+   
     await knex.schema.createTable(bookingTableName, (table) => {
         table.increments()
         table.date('date').notNullable()
@@ -113,20 +142,28 @@ export async function up(knex: Knex): Promise<void> {
         table.foreign('time').references('timeslot.id')
         table.timestamps(false, true)
         table.boolean('is_deleted').defaultTo(false)
-        table.integer('users_id').unsigned().notNullable()
-        table.foreign('users_id').references('users.id')
+        table.boolean('is_attended').defaultTo(false)
+        table.integer('user_id').unsigned().notNullable()
+        table.foreign('user_id').references('users.id')
         table.integer('dietitian_id').unsigned().notNullable()
         table.foreign('dietitian_id').references('dietitian.id')
     })
-
-    await knex.schema.createTable(userStatsTableName, (table) => {
+    
+    await knex.schema.createTable(userWeightTableName, (table) => {
         table.increments()
-        table.integer('weight').notNullable()
+        table.float('weight').notNullable()
         table.date('date').notNullable()
         table.timestamps(false, true)
-        table.integer('users_id').unsigned().notNullable()
-        table.foreign('users_id').references('users.id')
+        table.integer('user_id').unsigned().notNullable()
+        table.foreign('user_id').references('users.id')
     })
+
+    
+    await knex.schema.createTable(followupTableName, (table) => {
+        table.increments()
+        table.text('action').unique().notNullable()
+    })
+
 
     await knex.schema.createTable(dietitianReportTableName, (table) => {
         table.increments()
@@ -136,32 +173,34 @@ export async function up(knex: Knex): Promise<void> {
         table.float('weight').notNullable()
         table.float('blood_pressure').notNullable()
         table.float('blood_glucose').notNullable()
+        table.integer('follow_up').unsigned().notNullable()
+        table.foreign('follow_up').references('follow_up.id')
         table.timestamps(false, true)
-        table.integer('users_id').unsigned().notNullable()
-        table.foreign('users_id').references('users.id')
+        table.integer('user_id').unsigned().notNullable()
+        table.foreign('user_id').references('users.id')
         table.integer('dietitian_id').unsigned().notNullable()
         table.foreign('dietitian_id').references('dietitian.id')
     })
-
+   
     await knex.schema.createTable(usersBloodPressureTableName, (table) => {
         table.increments()
         table.integer('sys_bp').notNullable()
         table.integer('dia_bp').notNullable()
         table.date('date').notNullable()
         table.timestamps(false, true)
-        table.integer('users_id').unsigned().notNullable()
-        table.foreign('users_id').references('users.id')
+        table.integer('user_id').unsigned().notNullable()
+        table.foreign('user_id').references('users.id')
     })
-
+   
     await knex.schema.createTable(usersBloodGlucoseTableName, (table) => {
         table.increments()
         table.integer('bg_measurement').notNullable()
         table.date('date').notNullable()
         table.timestamps(false, true)
-        table.integer('users_id').unsigned().notNullable()
-        table.foreign('users_id').references('users.id')
+        table.integer('user_id').unsigned().notNullable()
+        table.foreign('user_id').references('users.id')
     })
-
+    
     await knex.schema.createTable(postTableName, (table) => {
         table.increments()
         table.text('content').notNullable()
@@ -173,7 +212,7 @@ export async function up(knex: Knex): Promise<void> {
         table.integer('author_id').unsigned().notNullable()
         table.foreign('author_id').references('dietitian.id')
     })
-
+   
     await knex.schema.createTable(commentsTableName, (table) => {
         table.increments()
         table.integer('post').unsigned().notNullable()
@@ -185,17 +224,17 @@ export async function up(knex: Knex): Promise<void> {
         table.integer('author').unsigned().notNullable()
         table.foreign('author').references('users.id')
     })
-
+    
     await knex.schema.createTable(dietTypesTableName, (table) => {
         table.increments()
         table.text('d_type').notNullable()
     })
-
+  
     await knex.schema.createTable(foodGroupsTableName, (table) => {
         table.increments()
         table.text('food_group').notNullable()
     })
-
+   
     await knex.schema.createTable(foodTableName, (table) => {
         table.increments()
         table.string('food_name').unique().notNullable()
@@ -209,7 +248,7 @@ export async function up(knex: Knex): Promise<void> {
         table.decimal('fiber', 8, 4).notNullable()
         table.decimal('sodium', 8, 4).notNullable()
     })
-
+   
     await knex.schema.createTable(usersDietTableName, (table) => {
         table.increments()
         table.integer('diet_type').unsigned().notNullable()
@@ -219,8 +258,8 @@ export async function up(knex: Knex): Promise<void> {
         table.decimal('food_amount', 5, 2).notNullable()
         table.date('date').notNullable()
         table.timestamps(false, true)
-        table.integer('users_id').unsigned().notNullable()
-        table.foreign('users_id').references('users.id')
+        table.integer('user_id').unsigned().notNullable()
+        table.foreign('user_id').references('users.id')
     })
 
 }
@@ -239,8 +278,9 @@ export async function down(knex: Knex): Promise<void> {
     await knex.schema.dropTableIfExists(timeSlotTableName)
     await knex.schema.dropTableIfExists(exercisesTableName)
     await knex.schema.dropTableIfExists(exercisesTypesTableName)
-    await knex.schema.dropTableIfExists(userStatsTableName)
+    await knex.schema.dropTableIfExists(userWeightTableName)
     await knex.schema.dropTableIfExists(userTableName)
+    await knex.schema.dropTableIfExists(followupTableName)
     await knex.schema.dropTableIfExists(chronicConditionTableName)
     await knex.schema.dropTableIfExists(dietitianTableName)
     await knex.schema.dropTableIfExists(educationName)
--- api/seed/seed.ts
+++ api/seed/seed.ts
@@ -1,4 +1,5 @@
 import type { Knex } from 'knex'
+import { hashPassword } from '../utilities/hash'
 
 const userTableName = 'users'
 const dietitianTableName = 'dietitian'
@@ -6,7 +7,7 @@ const genderTableName = 'gender'
 const professionTableName = 'profession_types'
 const educationName = 'education'
 const chronicConditionTableName = 'chronic_condition'
-const userStatsTableName = 'users_stats'
+const userWeightTableName = "users_weight"
 const dietitianReportTableName = 'dietitian_reports'
 const exercisesTableName = 'users_exercises'
 const exercisesTypesTableName = 'exercises_types'
@@ -36,7 +37,7 @@ export async function seed(knex: Knex) {
 	await knex(timeSlotTableName).del()
 	await knex(exercisesTableName).del()
 	await knex(exercisesTypesTableName).del()
-	await knex(userStatsTableName).del()
+	await knex(userWeightTableName).del()
 	await knex(userTableName).del()
 	await knex(chronicConditionTableName).del()
 	await knex(dietitianTableName).del()
@@ -44,21 +45,71 @@ export async function seed(knex: Knex) {
 	await knex(professionTableName).del()
 	await knex(genderTableName).del()
 
-	// const professionID = await knex(professionTableName)
-	// .insert([
-	// 	{pf_type:"IT"}
+	const professionID = await knex(professionTableName)
+	.insert([
+		{pf_type:"IT"},
+		{pf_type:"dietitian"}
 
-	// ]).returning('id')
+	]).returning('id')
 
-	// const genderID = await knex(genderTableName)
-	// .insert([
-	// 	{gender_type:"Male"},
-	// 	{gender_type:"Female"},
-	// 	{gender_type:"Others"}
-	// ])
+	const genderID = await knex(genderTableName)
+	.insert([
+		{gender_type:'Male'},
+		{gender_type:'Female'},
+		{gender_type:'Others'}
+	]).returning('id')
 
-	// const educationID = await knex(educationName)
-	// .insert([
-	// 	{education_level:""}
-	// ])
+	const educationID = await knex(educationName)
+	.insert([
+		{education_level:"Primary and below"},
+		{education_level:"Secondary"},
+		{education_level:"Post-secondary"}
+	]).returning('id')
+
+	const chronic_conditionID = await knex(chronicConditionTableName)
+	.insert([
+		{disease:"sleepy"}
+	])
+
+	const exercises_typesID = await knex(exercisesTypesTableName)
+	.insert([
+		{ex_type:"walk/run, moderate", ex_calories:290},
+		{ex_type:"walk/run, vigorous", ex_calories:363},
+		{ex_type:"Football", ex_calories:508},
+		{ex_type:"Basketball", ex_calories:581},
+		{ex_type:"Swimming, vigorous", ex_calories:726},
+		{ex_type:"Swimming, moderate", ex_calories:290},
+		{ex_type:"Climbing hills", ex_calories:545},
+		{ex_type:"Martial arts, kick boxing", ex_calories:726},
+		{ex_type:"Weight lifting", ex_calories:217},
+		{ex_type:"Cycling, moderate", ex_calories:581},
+		{ex_type:"Cycling, vigorous", ex_calories:726},
+		{ex_type:"Cycling, leisure", ex_calories:290},
+		{ex_type:"Whitewater rafting, kayaking, canoeing", ex_calories:363},
+		{ex_type:"Table tennis", ex_calories:290},
+		{ex_type:"Tennis", ex_calories:508}
+	]).returning('id')
+
+	const userID = await knex(userTableName)
+	.insert([
+		{username:"roy"},
+		{first_name:"hoi"},
+		{last_name:"chan"},
+		{email:"r@gmail.com"},
+		{password: await hashPassword('123')},
+		{birthday:'2000-20-20'},
+		{height:172},
+		{weight:68},
+		{gender:genderID[0].id},
+		{phone:12345678},
+		{address:"Ma On Shan"},
+		{profession:professionID[0].id},
+		{hkid:"Y7894568"},
+		{chronic_conditionID:null},
+		{education: educationID[1].id},
+		{is_deleted: false}
+
+
+	])
+	
 }
