5403c7a44a3dd085d5ae9c904cbaef10dfa609b1
SamOr1014 <orchamsumsam1014@gmail.com>
2022-09-22 16:26:07 +0800

    favicon added and added service worker

--- App/src/index.tsx
+++ App/src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./chakra/themeConfig";
@@ -25,6 +26,7 @@ root.render(
   // </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
