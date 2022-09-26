5d67500876980027f69608300ec69546009015f8
SamOr1014 <orchamsumsam1014@gmail.com>
2022-09-26 15:51:30 +0800

    adding iframe title

--- App/src/components/Data.tsx
+++ App/src/components/Data.tsx
@@ -8,6 +8,7 @@ export default function DataCenter() {
         ""
       ) : (
         <iframe
+          title="PowerBI_iframe"
           width="100%"
           height="100%"
           src="https://datastudio.google.com/embed/reporting/2b7c1066-4eb1-47b1-a127-1812eaedb8f9/page/p_5ixedkbyyc"
