079c96a207b257ca221bc022213a2e5284ececfc
Roy Chan <roychan170@gmail.com>
2022-09-26 13:29:34 +0800

    handled esline issue

--- App/src/components/Post.tsx
+++ App/src/components/Post.tsx
@@ -56,7 +56,7 @@ export default function UserPost() {
           type: "paragraph",
           children: [{ text: "" }],
         },
-      ],
+      ],// eslint-disable-next-line react-hooks/exhaustive-deps
     [isOpen]
   );
 
@@ -67,6 +67,7 @@ export default function UserPost() {
           type: "paragraph",
           children: [{ text: "" }],
         },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
       ],
     [isOpen]
   );
