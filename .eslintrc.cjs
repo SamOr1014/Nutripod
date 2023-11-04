7f74d4b441bfa79dc558e175e979613a01b9fe62
SamOr1014 <orchamsumsam1014@gmail.com>
2023-11-04 18:31:27 +0800

    chore: remove post route for first time deployment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ const config = {
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
+    "@typescript-eslint/require-await": "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
 
@@ -23,7 +24,8 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": "off",
+    // "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-misused-promises": [
       2,
       {
