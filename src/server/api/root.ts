9ac921f029ad03f060c2a8cd1418d2e1bf3d0934
SamOr1014 <orchamsumsam1014@gmail.com>
2023-11-04 18:10:59 +0800

    chore: remove post route for first time deployment

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -6,9 +6,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({
-  post: postRouter,
-});
+export const appRouter = createTRPCRouter({});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
